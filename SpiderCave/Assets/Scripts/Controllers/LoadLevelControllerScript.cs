950142efd070db6cfe9f34782920d72a29120a6d
Ashwin <ashwinhole06@gmail.com>
2018-12-03 22:22:54 -0700

    Add LevelScene and MainMenuScene With Functionality

---
+++ SpiderCave/Assets/Scripts/Controllers/LoadLevelControllerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelControllerScript : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
